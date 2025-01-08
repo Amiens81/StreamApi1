917649e19627fa69d03da929b0427a1e79659d04
Amiens81 <ypovelica@ya.ru>
2025-01-08 17:35:45 +0300

    Русификация toString

--- src/SecondTask/Person.java
+++ src/SecondTask/Person.java
@@ -20,8 +20,8 @@ public class Person {
 	@Override
 	public String toString() {
 		return "Person{" +
-				"name='" + name + '\'' +
-				", age=" + age +
+				"Имя='" + name + '\'' +
+				", Возраст=" + age +
 				'}';
 	}
 }
